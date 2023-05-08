0b5a5d0ca86b75d7a265f56edd70c26cffba51ed
Quentin <quentintaranpino@proton.me>
2023-05-08 16:09:01 +0200

    Automatic populate tables

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import cors from "cors";
 import express from "express";
 import { createClient } from "redis";
+import { populateTables } from "./database";
 
 import { loadApiEndpoints } from "./controllers/index.api";
+import { logger } from "./logger";
 
 // Express configuration
 const app = express();
@@ -17,9 +19,16 @@ app.use(cors());
 //Redis configuration
 const redisClient = createClient();
 (async (): Promise<void> => {
-	redisClient.on("error", (error) => console.error(`Error : ${error}`));
+	redisClient.on("error", (error) => logger.error(`There is a problem connecting to redis server : ${error}`));
 	await redisClient.connect();
 })();
+
+//Check database tables
+const dbtables = populateTables(false);
+if (!dbtables) {
+	console.error("Error creating database tables");
+	process.exit(1);
+}
 
 //Load API endpoints
 loadApiEndpoints(app);
--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import { createPool, Pool } from "mysql2/promise";
+import { logger } from "./logger";
 
 export async function connect(): Promise<Pool> {
 	const connection = await createPool({
@@ -11,4 +12,47 @@ export async function connect(): Promise<Pool> {
 	});
 
 	return connection;
+}
+
+export async function populateTables(resetTables : boolean): Promise<boolean>{
+
+	if (resetTables){
+	//Drop registered table
+	const conn = await connect();
+	logger.info("Dropping table registered");
+	let RegisteredTableDropStatement : string =	"DROP TABLE IF EXISTS registered;"
+	await conn.query(RegisteredTableDropStatement);
+	conn.end();
+	}
+
+	//Create registered table
+	const conn = await connect();
+	let ExistRegisteredTableStatement : string = "SHOW TABLES FROM `nostrcheck` LIKE 'registered';"
+	logger.info("Checking if table registered exist")
+	const [ExistRegisteredTable] = await conn.query(ExistRegisteredTableStatement);
+	const rowstemp = JSON.parse(JSON.stringify(ExistRegisteredTable));
+	if(rowstemp[0] == undefined){
+
+		let RegisteredTableCreateStatement : string =
+		"CREATE TABLE IF NOT EXISTS registered ("+
+		"id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,"+
+		"pubkey varchar(64) NOT NULL,"+
+		"hex varchar(64) NOT NULL,"+
+		"username varchar(50) NOT NULL,"+
+		"password varchar(100) NOT NULL,"+
+		"domain varchar(50) NOT NULL,"+
+		"active boolean NOT NULL DEFAULT 0,"+
+		"date datetime NOT NULL,"+
+		"comments varchar(150)"+
+		") ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+
+		logger.info("Creating table registered");
+
+		await conn.query(RegisteredTableCreateStatement);
+		conn.end();
+	}else{
+		logger.info("Table registered alredy exist, skipping creation");
+	}	
+
+return true;
 }
