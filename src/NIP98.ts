49d469daa2830c9d94c079c7556fc71fedfad6aa
Quentin <quentintaranpino@proton.me>
2023-05-08 10:13:29 +0200

    NIP98 verification methods

--- src/NIP98.ts
+++ src/NIP98.ts
@@ -30,6 +30,19 @@ interface AuthEventResult {
         
     try {
         const authevent = JSON.parse(Buffer.from(req.headers.authorization?.substring(6, req.headers.authorization.length), 'base64').toString('utf8'));
+
+        //Check if event authorization content is valid
+        let IsAuthEventValid = VerifyAuthEvent(authevent)
+        if(IsAuthEventValid.result == false){
+            logger.warn(
+                "RES -> 400 Bad request - " + IsAuthEventValid.description,
+                "|",
+                req.socket.remoteAddress
+            );
+            let result : AuthEventResult = {result: false, description: "Authorization header invalid"}
+            return result;
+        }
+
         let result : AuthEventResult = {result: true, description: "Authorization header valid"}
         return result;
         
@@ -43,10 +56,49 @@ interface AuthEventResult {
             return result;
         }
 
-
-    //FALTA PREPARAR LOS METODOS PARA VERIFICAR QUE EFECTIVAMENTE LA FIRMA ES VALIDA Y QUE EL EVENTO ES VALIDO
 }
 
 export { ParseAuthEvent }
 
+const VerifyAuthEvent =  (req: Request): AuthEventResult => {
 
+//Check if event authorization kind is valid (Must be 27235)
+const kind = req.body.kind
+if (kind == null || kind == undefined || kind != "27235") {
+    logger.warn(
+        "RES -> 400 Bad request - Auth header event kind is not 27235",
+        "|",
+        req.socket.remoteAddress
+    );
+    let result : AuthEventResult = {result: false, description: "Auth header event kind is not 27235"}
+    return result;
+}
+
+//Check if event authorization tags are valid
+const endpoint = req.body.tags[0][1]
+const servername = req.hostname;
+if (endpoint == null || endpoint == undefined || endpoint != servername) {
+    logger.warn(
+        "RES -> 400 Bad request - Auth header event endpoint is not valid",
+        "|",
+        req.socket.remoteAddress
+    );
+    let result : AuthEventResult = {result: false, description: "Auth header event endpoint is valid"}
+    return result;
+}
+
+const method = req.body.tags[1][1]
+const receivedmethod = req.method;
+if (method == null || method == undefined || method != receivedmethod) {
+    logger.warn(
+        "RES -> 400 Bad request - Auth header event method is not valid",
+        "|",
+        req.socket.remoteAddress
+    );
+    let result : AuthEventResult = {result: false, description: "Auth header event method is not valid"}
+    return result;
+}
+
+return {result: true, description: "Auth header event is valid"}
+
+}
