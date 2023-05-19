cb3f9fd1ba42cb64bdf3b60f563e2db1f9b8c933
Quentin <quentintaranpino@proton.me>
2023-05-19 10:41:48 +0200

    new media endpoint

---
+++ src/controllers/media.ts
@@ -0,0 +1,92 @@
+import { Application, Request, Response } from "express";
+import { Event, getEventHash, nip19, validateEvent, verifySignature } from "nostr-tools";
+
+import { connect } from "../database";
+import { logger } from "../logger";
+import { ParseAuthEvent } from "../NIP98";
+
+import { allowedMimeTypes, MediaResultMessage, ResultMessage, UploadTypes } from "../types";
+
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 100 * 1024 * 1024 } //100MB max file size
+});
+
+export const LoadMediaEndpoint = (app: Application): void => {
+	app.post("/api/v1/media", upload.single('media'), async (req: Request, res: Response): Promise<Response> => {
+
+        logger.info("POST /api/v1/media", "|", req.socket.remoteAddress);
+
+        //Check if event authorization header is valid (NIP98)
+		const EventHeader = ParseAuthEvent(req);
+		if (!EventHeader.result) {
+			logger.warn(
+				`RES -> 400 Bad request - ${EventHeader.description}`,
+				"|",
+				req.socket.remoteAddress
+			);
+			const result: ResultMessage = {
+				result: false,
+				description: EventHeader.description,
+			};
+
+			return res.status(400).send(result);
+		}
+
+        //Check if upload type exists
+        const uploadtype = req?.body.type;
+        if (!uploadtype){
+            logger.warn(`RES -> 400 Bad request - missing upload type`, "|", req.socket.remoteAddress);
+            const result: ResultMessage = {
+                result: false,
+                description: "missing upload type",
+            };
+            return res.status(400).send(result);
+        }
+        //Check if upload type is valid
+        if(!UploadTypes.includes(uploadtype)){
+            logger.warn(`RES -> 400 Bad request - incorrect upload type`, "|", req.socket.remoteAddress);
+            const result: ResultMessage = {
+                result: false,
+                description: "incorrect upload type",
+            };
+            return res.status(400).send(result);
+        }
+        logger.info("type ->", uploadtype, "|", req.socket.remoteAddress);
+
+        //Check if file exist
+        const file = req.file;
+        if (!file) {
+
+            logger.warn(`RES -> 400 Bad request - Empty file`, "|", req.socket.remoteAddress);
+			const result: ResultMessage = {
+				result: false,
+				description: "Empty file",
+			};
+            return res.status(400).send(result);
+        }
+        //Check if filetype is allowed
+        if(!allowedMimeTypes.includes(file.mimetype)){
+            logger.warn(`RES -> 400 Bad request - Incorrect file type`, "|", req.socket.remoteAddress);
+            const result: ResultMessage = {
+                result: false,
+                description: "Incorrect file type",
+            };
+            return res.status(400).send(result);
+        }
+        logger.info("mime ->", file.mimetype, "|", req.socket.remoteAddress);
+
+
+        //RETURN FILE URL
+        logger.info(`RES -> 200 OK - File uploaded successfully`, "|", req.socket.remoteAddress);
+        const result: MediaResultMessage = {
+            url: "FILENAME",
+            result: true,
+            description: "File uploaded successfully",
+        };
+        return res.status(200).send(result);
+
+    });
+}
