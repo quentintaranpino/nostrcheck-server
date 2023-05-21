3d1c22ac54f0bc6f874b988c5b910f69b21733b1
Quentin <quentintaranpino@proton.me>
2023-05-21 19:37:33 +0200

    merge changes

--- src/routes/register.route.ts
+++ src/routes/register.route.ts
@@ -3,8 +3,4 @@ import {Registernewpubkey} from "../controllers/register"
 
 export const LoadRegisterEndpoint = async (app: Application): Promise<void>=> {
 	app.post("/api/v1/register", Registernewpubkey);
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 54fdac2a552bca94e3dbf2890fe4ef29f35d5ba1
