40292a0c7b83633776c634e673c2d0e9135e8537
Quentin <quentintaranpino@proton.me>
2023-05-22 12:46:46 +0200

    lint

--- src/routes/verify.route.ts
+++ src/routes/verify.route.ts
@@ -3,7 +3,7 @@ import { Application } from "express";
 import { VerifyNote } from "../controllers/verify";
 
 export const LoadVerifyEndpoint = async (app: Application): Promise<void> => {
-	 app.post("/api/v1/verify", VerifyNote);
+	app.post("/api/v1/verify", VerifyNote);
 
 	// app.get("/api/v1/verify", (_req, res) => {
 	// 	res.status(200).send("Verify endpoint");
