bc6835e11623d54b6c466925fa9f0a56e656243a
Quentin <quentintaranpino@proton.me>
2023-05-31 12:58:15 +0200

    new config lib

---
+++ src/lib/config.ts
@@ -0,0 +1,62 @@
+import config from "config";
+import {logger} from "./logger.js";
+import fs from "fs";
+import { exit } from "process";
+import { json } from "body-parser";
+
+
+function prepareAppFolders(){
+
+	let TempPath : string = config.get("media.tempPath");
+	logger.info("Cleaning temp dir");
+
+	//If not exist create temp folder
+	if (!fs.existsSync(TempPath)){
+		fs.mkdirSync(TempPath);
+	}
+
+	fs.readdir(TempPath, (err, files) => {
+		if (err) {
+			logger.fatal(err);
+            exit(1);
+		}
+
+		for (const file of files) {
+			fs.unlink(TempPath + file, (err) => {
+				if (err) {
+                    logger.error(err);
+                    exit(1);
+				}
+			});
+		}
+	});
+
+	//If not exist create media folder
+	const MediaPath : string = config.get("media.mediaPath");
+	if (!fs.existsSync(MediaPath)){
+		fs.mkdirSync(MediaPath);
+	}
+
+}
+
+function prepareAPPConfig(){
+
+    const DefaultPath : string = "./config/default.json";
+    const ConfigPath : string = "./config/config.json";
+
+    //If config file exist return
+	if (fs.existsSync(ConfigPath)){
+		return
+	}
+
+    fs.copyFile(DefaultPath, ConfigPath, function (err) {
+        if (err) {
+            logger.fatal("An error occured while writing config JSON File.", err);
+            exit(1);
+        }
+     
+        logger.info("Creating config file: " + ConfigPath)
+    });
+}
+
+export { prepareAppFolders, prepareAPPConfig };
