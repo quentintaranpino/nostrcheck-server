34a3414dfd234a3e26e89836facf74dd6204b234
quentintaranpino <quentintaranpino@proton.me>
2023-06-29 21:47:44 +0200

    lint nostraddress

--- src/controllers/nostraddress.ts
+++ src/controllers/nostraddress.ts
@@ -48,10 +48,7 @@ const Checknostraddress = async (req: Request, res: Response): Promise<Response>
 	// Root _ pubkey
 	const rootkey : string = config.get('server.pubkey'); 
 	if (req.query.name === "_") {
-		const result: RegisteredUsernameResult = { username: "_", hex: rootkey };
-
 		return res.status(200).send(JSON.stringify({ names: { ['_']: rootkey } }));
-
 	}
 
 	const result: RegisteredUsernameResult = { username: "", hex: "" };
