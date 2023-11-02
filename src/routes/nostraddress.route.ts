e4cfa6608962857ded5c6c980da055ae66eb4349
Quentin <quentintaranpino@proton.me>
2023-11-02 07:22:43 +0100

    Fix #6 Potential issue with NIP-05 route

--- src/routes/nostraddress.route.ts
+++ src/routes/nostraddress.route.ts
@@ -4,8 +4,8 @@ import { Checknostraddress } from "../controllers/nostraddress.js";
 
 export const LoadNostraddressEndpoint = async (app: Application, version:string): Promise<void> => {
 
-	if (version == "v1"){
-		app.get("/api/v1/nostraddress", Checknostraddress);
+	if (version == "v1" || version == "v2"){
+		app.get("/api/" + version + "/nostraddress", Checknostraddress);
 	}
 
 };
