05d8090d6318763e5c262922334d03105fdf2eb5
= <=>
2024-01-19 14:23:41 +0100

    frontend routes refactor

--- src/routes/frontend.route.ts
+++ src/routes/frontend.route.ts
@@ -1,37 +1,52 @@
 import { Application } from "express";
-import { Session } from "express-session";
+import { adminLogin } from "../controllers/admin.js";
+
+
+declare module 'express-session' {
+	interface Session {
+	   username: string;
+	 }
+	}
 
 export const LoadFrontendEndpoint = async (app: Application, _version:string): Promise<void> => {
 
+	//Legacy routes
 	app.get("/", (_req, res) => {
 		res.redirect("/api/v2");
 	});
 	app.get("/api", (_req, res) => {
 		res.redirect("/api/v2");
 	});
-
 	app.get("/api/v1", (_req, res) => {
 		res.redirect("/api/v2");
 	});
 
-	//Frontend
+	//Current v2 routes
+	app.get("/api/v2", (req, res) => {
+		if (req.session.username == null){
+			res.redirect('/login');
+		}else{
+		res.render("index.ejs", {request: req});
+		}
+	});
+
+	//Login
 	app.get("/login", (req, res) => {
 		res.render("login.ejs", {request: req});
 	});
-
-	app.get("/api/v2", (req, res, next) => {
-		if (req.session == null){
-			res.redirect('/login');
-		}   else{
-			next();
-		}
-		res.render("index.ejs", {request: req});
+	app.post("/login", (req, res) => {
+		adminLogin(req).then((result) => {
+			if (result){
+				res.redirect('/api/v2');
+			}else{
+				res.redirect('/login');
+			}
+		});
 	});
+
+	//Tos
 	app.get("/tos", (req, res) => {
 		res.render("tos.ejs", {request: req});
 	});
-
-
-
 
 };
