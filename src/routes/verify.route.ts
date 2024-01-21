d4dbbeb21a461d24bccfd06795a3ac152380bd3c
= <=>
2024-01-21 16:53:36 +0100

    refactor verify module

--- src/routes/verify.route.ts
+++ src/routes/verify.route.ts
@@ -1,11 +1,11 @@
 import { Application } from "express";
 
-import { VerifyNote } from "../controllers/verify.js";
+import { verifyEventController } from "../controllers/verify.js";
 
 export const LoadVerifyEndpoint = async (app: Application, version:string): Promise<void> => {
 
 	if (version == "v1"){
-	app.post("/api/v1/verify", VerifyNote);
+	app.post("/api/v1/verify", verifyEventController);
 	}
 
 };
