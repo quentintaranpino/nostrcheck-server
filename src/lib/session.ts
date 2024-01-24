75ba3b71a5647fa58df55584a874530b268849f8
quentintaranpino <quentintaranpino@proton.me>
2024-01-24 20:34:41 +0100

    update DDOS limit to 1000 (gallery fails to load)

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -62,7 +62,7 @@ const checkSessionSecret = async(): Promise<string> => {
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 })
