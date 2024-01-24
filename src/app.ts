c00bbe3031eb5f6b7f0f28b91da9ce88ba9d7611
quentintaranpino <quentintaranpino@proton.me>
2024-01-24 07:42:47 +0100

    torrent seeding control from config file

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,20 @@ import express from "express";
 import helmet from "helmet";
 import config from "config";
 import initSession from "./lib/session.js";
-import { prepareAPPConfig, prepareAppFolders, loadconfigEndpoints  } from "./lib/config.js";
+import { prepareAPP, loadconfigEndpoints  } from "./lib/config.js";
 
-import { LoadAPI } from "./routes/routes.js";
+import { loadAPIs } from "./routes/routes.js";
+import { initDatabase } from "./lib/database.js";
+import { SeedMediafilesMagnets } from "./lib/torrent.js";
 
-async function prepareAPP() {
-    await prepareAPPConfig();
-	await prepareAppFolders();
-}
-
+// Initialise config and folders
 await prepareAPP();
 
-const app = express();
+// Initialise Database
+await initDatabase();
 
+// Initialise Express
+const app = express();
 app.set("host", process.env.HOSTNAME ?? config.get('server.host'));
 app.set("port", process.env.PORT ?? config.get('server.port'));
 app.set("version", process.env.npm_package_version ?? "0.0");
@@ -27,6 +28,7 @@ app.set("activeEndpoints", await loadconfigEndpoints());
 app.set('trust proxy',true); 
 app.set("view engine", "ejs")
 app.set('views','./src/pages/');
+
 app.use(express.json({ limit: '25MB' }));
 app.use(express.urlencoded({ limit: '25MB', extended: true }));
 app.use(express.static('./src/pages/'));
@@ -36,10 +38,10 @@ app.use(cors());
 // Initialise session cookies
 await initSession(app);
 
-const loadAPIs = async () => {
-	await LoadAPI(app, "v1");
-	await LoadAPI(app, "v2");
-}
-loadAPIs();
+// Initialise API endpoints
+await loadAPIs(app);
+
+//Start seeding magnets
+if (config.get("server.enableTorrentSeeding")) {SeedMediafilesMagnets();}
 
 export default app;
