2fe0806f5e17856edca743975e56c1bc6ac003f1
Quentin <quentintaranpino@proton.me>
2024-01-25 12:23:13 +0100

    remove comments from session lib

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -62,9 +62,9 @@ const checkSessionSecret = async(): Promise<string> => {
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	limit: 1000, 
+	standardHeaders: 'draft-7',
+	legacyHeaders: false, 
 })
 
 export { initSession, limiter };
