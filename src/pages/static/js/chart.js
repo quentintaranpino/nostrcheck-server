2996fc9e7fa577b4cab6813ae1936f1b89111a2c
quentintaranpino <quentintaranpino@proton.me>
2024-02-03 22:03:12 +0100

    chart multiple years WIP

--- src/pages/static/js/chart.js
+++ src/pages/static/js/chart.js
@@ -1,22 +1,37 @@
 const initMonthChart = (chartId, label, data) => {
   const rawData = JSON.parse(data);
-  const monthCounts = new Array(12).fill(0);
+  const monthCountsCurrentYear = new Array(12).fill(0);
+  const monthCountsLastYear = new Array(12).fill(0);
   rawData.forEach(item => {
     const date = new Date(item.date.toString().substring(0,10));
-    console.debug('Date:', item.date.toString().substring(0,10));
-    const month = date.getUTCMonth(); 
-    monthCounts[month]++; 
+    if (date.getUTCFullYear() == new Date().getFullYear()) {
+      const month = date.getUTCMonth(); 
+      monthCountsCurrentYear[month]++; 
+    }
+    if (date.getUTCFullYear() == new Date().getFullYear()-1) {
+      const month = date.getUTCMonth(); 
+      monthCountsLastYear[month]++; 
+    }
   });
-  const labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11','12']
+  const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec']
   new Chart($(chartId), {
     type: 'bar',
+    label: label,
     data: {
       labels: labels,
       datasets: [{
-        label: label, 
-        data: monthCounts,
+        label: '2024', 
+        data: monthCountsCurrentYear,
         borderColor: '#8E9396',
         backgroundColor: '#9CA2A7',
+        cubicInterpolationMode: 'monotone',
+        tension: 0.4,
+        borderWidth: 3
+      }, {
+        label: '2023', 
+        data: monthCountsLastYear,
+        borderColor: '#FF5733',
+        backgroundColor: '#FF5733',
         cubicInterpolationMode: 'monotone',
         tension: 0.4,
         borderWidth: 3
