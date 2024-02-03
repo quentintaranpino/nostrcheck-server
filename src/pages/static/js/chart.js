067854e303a06a9d23387f4892eccf5544ce6e16
quentintaranpino <quentintaranpino@proton.me>
2024-02-03 22:12:29 +0100

    chartjs wip

--- src/pages/static/js/chart.js
+++ src/pages/static/js/chart.js
@@ -1,4 +1,4 @@
-const initMonthChart = (chartId, label, data) => {
+const initMonthChart = (chartId, title, data) => {
   const rawData = JSON.parse(data);
   const monthCountsCurrentYear = new Array(12).fill(0);
   const monthCountsLastYear = new Array(12).fill(0);
@@ -16,22 +16,21 @@ const initMonthChart = (chartId, label, data) => {
   const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec']
   new Chart($(chartId), {
     type: 'bar',
-    label: label,
     data: {
       labels: labels,
       datasets: [{
-        label: '2024', 
+        label: new Date().getFullYear(), 
         data: monthCountsCurrentYear,
-        borderColor: '#8E9396',
-        backgroundColor: '#9CA2A7',
+        borderColor: '#A575FF',
+        backgroundColor: '#A575FF',
         cubicInterpolationMode: 'monotone',
         tension: 0.4,
         borderWidth: 3
       }, {
-        label: '2023', 
+        label: new Date().getFullYear()-1,
         data: monthCountsLastYear,
-        borderColor: '#FF5733',
-        backgroundColor: '#FF5733',
+        borderColor: '#8E9396',
+        backgroundColor: '#9CA2A7',
         cubicInterpolationMode: 'monotone',
         tension: 0.4,
         borderWidth: 3
@@ -39,6 +38,12 @@ const initMonthChart = (chartId, label, data) => {
     },
     options: {
       responsive: true,
+      plugins: {
+        title: {
+            display: true,
+            text: title + ' (Last 2 Years)'
+        }
+      },
       scales: {
         y: {
           beginAtZero: true
