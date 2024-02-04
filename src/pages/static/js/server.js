dbe971cd14a3360ae55b72a382dc3510396e943b
quentintaranpino <quentintaranpino@proton.me>
2024-02-04 18:36:54 +0100

    fix server.js for uptime

--- src/pages/static/js/server.js
+++ src/pages/static/js/server.js
@@ -2,7 +2,10 @@
     fetch(window.location.protocol + "//" + window.location.host + '/api/v2/admin/status')
     .then(res => res.json())
     .then(out =>
-      document.getElementById('server-uptime').innerHTML = out.uptime)
+      {let uptimes = document.getElementsByClassName('server-uptime');
+      Array.prototype.forEach.call(uptimes, function(uptime) {
+        uptime.innerHTML = out.uptime;
+      })})
     .catch(err => { throw err });
     setTimeout(getServerUptime, 1000);
   }
