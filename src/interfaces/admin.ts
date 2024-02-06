0ca633aca9af35b0056fced05a1fa6c0bead3e7b
quentintaranpino <quentintaranpino@proton.me>
2024-02-06 21:15:43 +0100

    refactor admin interfaces

---
+++ src/interfaces/admin.ts
@@ -0,0 +1,17 @@
+    // Define a list of allowed table names and field names
+    const allowedTableNames = ["registered", "mediafiles", "lightning", "domains"];
+    const allowedFieldNames = ["allowed", "active", "visibility", "comments", "username", "pubkey", "hex", "domain", "checked"]; 
+
+    const allowedFieldNamesAndValues = [
+        {field: "allowed", values: [0, 1]},
+        {field: "active", values: [0, 1]},
+        {field: "visibility", values: [0, 1]},
+        {field: "checked", values: [0, 1]},
+        {field: "comments", values: ["string"]},
+        {field: "username", values: ["string"]},
+        {field: "pubkey", values: ["string"]},
+        {field: "hex", values: ["string"]},
+        {field: "domain", values: ["string"]}
+    ];
+
+    export { allowedTableNames, allowedFieldNames, allowedFieldNamesAndValues };
