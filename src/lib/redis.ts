8635a02eb80ebf26f559a7f0e38dd4a6390b3602
Kirino Minato <kirino.minato@gmail.com>
2024-02-13 02:58:40 +0000

    Add Redis hostname setting to default.json

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,11 +1,16 @@
 import { createClient } from "redis";
+import config from "config";
 
 import { logger } from "../lib/logger.js";
 import { RegisteredUsernameResult } from "../interfaces/register.js";
 import { LightningUsernameResult } from "../interfaces/lightning.js";
 
 //Redis configuration
-const redisClient = createClient();
+const RedisHost: string = config.get('redis.host');
+const RedisPort: string = config.get('redis.port');
+const RedisUser: string = config.get('redis.user');
+const RedisPassword: string = config.get('redis.password');
+const redisClient = createClient({ url: `redis://${RedisUser}:${RedisPassword}@${RedisHost}:${RedisPort}` });
 (async (): Promise<void> => {
 	redisClient.on("error", (error: any) =>{
 		logger.error(`There is a problem connecting to redis server, is redis-server package installed on your system? : ${error}`);
