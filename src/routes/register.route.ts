f66193f0c9e06d9213b87362a3a4fdd9cd9437dd
quentintaranpino <quentintaranpino@proton.me>
2024-02-16 10:05:44 +0100

    lint register route

--- src/routes/register.route.ts
+++ src/routes/register.route.ts
@@ -1,11 +1,11 @@
 import { Application } from "express";
 
-import { Registernewpubkey } from "../controllers/register.js";
+import { registernewpubkey } from "../controllers/register.js";
 
 export const loadRegisterEndpoint = async (app: Application, version:string): Promise<void> => {
 
 	if (version == "v1" || version == "v2"){
-		app.post("/api/" + version + app.get("activeModules")["register"]["path"], Registernewpubkey);
+		app.post("/api/" + version + app.get("activeModules")["register"]["path"], registernewpubkey);
 	}
 	
 };
