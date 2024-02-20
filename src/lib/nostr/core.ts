a846878b04bb3ef864aba64f4093569b81342341
quentintaranpino <quentintaranpino@proton.me>
2024-02-20 16:37:44 +0100

    new method getProfileMetadata

--- src/lib/nostr/core.ts
+++ src/lib/nostr/core.ts
@@ -1,9 +1,10 @@
-import { generateSecretKey, getPublicKey, nip19 } from "nostr-tools";
+import { Event, generateSecretKey, getPublicKey, nip19 } from "nostr-tools";
 import { bytesToHex } from '@noble/hashes/utils'
 import { NostrEvent } from "nostr-tools"
 import {SimplePool } from "nostr-tools/pool"
 import 'websocket-polyfill'
 import { logger } from "../logger.js"
+import { E } from "vitest/dist/reporters-LLiOBu3g.js";
 
 const relays = [
 	"wss://relay.nostrcheck.me",
@@ -12,6 +13,8 @@ const relays = [
 	"wss://nos.lol",
 	"wss://relay.primal.net"
 	]
+
+const relaysPool = new SimplePool()
 
 /**
  * Generates a key pair consisting of a public key and a secret key.
@@ -51,7 +54,6 @@ const getPubkeyFromSecret = async (secretKey : string) : Promise<string> => {
 const publishEvent = async (event : NostrEvent): Promise<boolean> => {
 
         try{
-                const relaysPool = new SimplePool()
                 await Promise.any(relaysPool.publish(relays, event))
                 return true
         }catch (error) {
@@ -61,7 +63,41 @@ const publishEvent = async (event : NostrEvent): Promise<boolean> => {
         
 }
 
+/**
+ * Retrieves the profile data of a user from the Nostr network (Kind 0).
+ * @param pubkey - The public key of the user, hex format.
+ * @returns A promise that resolves to the content data of the kind 0 note.
+ */
+const getProfileMetadata = async (pubkey : string) : Promise<Event> => {
+	
+    let resolveEvent : (event : Event) => void;
+    let subscribePromise : Promise<Event> = new Promise(resolve => resolveEvent = resolve);
+    
+    const data = relaysPool.subscribeMany(
+		relays,
+		[{
+			authors: [pubkey],
+			kinds: [0],
+		}],
+		{
+			eoseTimeout: 1000,
+			onevent(e) {
+				resolveEvent(e);
+			},
+			oneose() {
+				data.close();
+				return resolveEvent(JSON.parse({kind: 0, created_at: 0, tags: [], content: "{}", pubkey: "", id: "", sig: ""}.content));
+			},
+		},
+	);
 
-export {publishEvent, createkeyPair, getPubkeyFromSecret}
+    let event : Event = await subscribePromise;
+	if (event.content === undefined) {
+		return JSON.parse({kind: 0, created_at: 0, tags: [], content: "{}", pubkey: "", id: "", sig: ""}.content);
+	}
+	return JSON.parse(event.content);
+}
+
+export {publishEvent, createkeyPair, getPubkeyFromSecret, getProfileMetadata}
 
 
