0c0aad67812eff537bb3b4e4b45203a9844c2246
quentintaranpino <quentintaranpino@proton.me>
2024-02-23 14:50:11 +0100

    refactor redis app.set from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
-import config from "config";
 import { loadConfigOptions } from "./lib/config.js";
 
 const app = express();
@@ -9,21 +8,17 @@ const app = express();
 app.set("config.server", await loadConfigOptions("server"));
 app.set("config.media", await loadConfigOptions("media"));
 app.set("config.logger", await loadConfigOptions("logger"));
-
-app.set("redis.host", config.get('redis.host'));
-app.set("redis.port", config.get('redis.port'));
-app.set("redis.user", config.get('redis.user'));
-app.set("redis.password", config.get('redis.password'));
+app.set("config.redis", await loadConfigOptions("redis"));
 
 app.set("version", process.env.npm_package_version ?? "0.0");
 
 app.set('trust proxy', 1); 
 app.set("view engine", "ejs")
-app.set('views','./src/pages/');
+app.set('views','./src/pages/'); // TODO 0.6.0 Theming the frontend with this line
 
 app.use(express.json({ limit: '25MB' }));
 app.use(express.urlencoded({ limit: '25MB', extended: true }));
-app.use(express.static('./src/pages/'));
+app.use(express.static('./src/pages/')); // TODO 0.6.0 Theming the frontend with this line
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 
