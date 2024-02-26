bfea074ea285c06ba8d72e7f00d87b0ebaf9cb38
quentintaranpino <quentintaranpino@proton.me>
2024-02-26 11:20:00 +0100

    userMetadata interface

--- src/interfaces/frontend.ts
+++ src/interfaces/frontend.ts
@@ -2,4 +2,19 @@ interface registeredTableResponse {
 	usernames: [JSON];
 }
 
-export { registeredTableResponse };
+interface userMetadata {
+	about : string;
+	banner : string;
+	display_name : string;
+	followers : number;
+	following : number;
+	lud16 : string;
+	mediaFiles : string[];
+	name : string;
+	nip05 : string;
+	picture : string;
+	username : string;
+	website : string;
+}
+
+export { registeredTableResponse, userMetadata };
