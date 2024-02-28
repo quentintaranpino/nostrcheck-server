b95f3f32b98b7ad31bb7f8b895e63d798395b389
quentintaranpino <quentintaranpino@proton.me>
2024-02-28 14:16:45 +0100

    add fields from lightning table to allowedFieldNames

--- src/interfaces/admin.ts
+++ src/interfaces/admin.ts
@@ -1,21 +1,20 @@
-import { ResultMessagev2 } from "./server.js";
 
-    // Define a list of allowed table names and field names
-    const allowedTableNames = ["registered", "mediafiles", "lightning", "domains"];
-    const allowedFieldNames = ["allowed", "active", "visibility", "comments", "username", "pubkey", "hex", "domain", "checked"]; 
+const allowedTableNames = ["registered", "mediafiles", "lightning", "domains"];
+const allowedFieldNames = ["allowed", "active", "visibility", "comments", "username", "pubkey", "hex", "domain", "checked", "lightningaddress"]; 
 
-    const allowedFieldNamesAndValues = [
-        {field: "allowed", values: [0, 1]},
-        {field: "active", values: [0, 1]},
-        {field: "visibility", values: [0, 1]},
-        {field: "checked", values: [0, 1]},
-        {field: "comments", values: ["string"]},
-        {field: "username", values: ["string"]},
-        {field: "pubkey", values: ["string"]},
-        {field: "hex", values: ["string"]},
-        {field: "domain", values: ["string"]},
-        {field: "id", values: ["number"]},
-        {field: "date", values: ["string"]}
-    ];
+const allowedFieldNamesAndValues = [
+    {field: "allowed", values: [0, 1]},
+    {field: "active", values: [0, 1]},
+    {field: "visibility", values: [0, 1]},
+    {field: "checked", values: [0, 1]},
+    {field: "comments", values: ["string"]},
+    {field: "username", values: ["string"]},
+    {field: "pubkey", values: ["string"]},
+    {field: "hex", values: ["string"]},
+    {field: "domain", values: ["string"]},
+    {field: "id", values: ["number"]},
+    {field: "date", values: ["string"]},
+    {field: "lightningaddress", values: ["string"]},
+];
 
-    export { allowedTableNames, allowedFieldNames, allowedFieldNamesAndValues };
+export { allowedTableNames, allowedFieldNames, allowedFieldNamesAndValues };
