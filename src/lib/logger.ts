12083000d547c32b84357fc78c1df5de3ebb1f8b
quentintaranpino <quentintaranpino@proton.me>
2024-03-02 16:00:23 +0100

    add try catch to logger lib

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -43,20 +43,25 @@ const logger = new Logger({
 });
 
 logger.attachTransport((log) => {
-	stream.write(`${JSON.stringify(log)}\n`);
-	// only push to transports if logLevel is greater than or equal to 4 (warn)
-	if (log._meta.logLevelId >= 4) {
-		const logEvent: logEvent = {
-			date : log._meta.date,
-			severity: log._meta.logLevelName,
-			message: log['0'].toString(),
-		};
-		logHistory.push(logEvent);
-		// Keep only the last 1000 lines in logHistory
-		if (logHistory.length > 1000) {
-			logHistory.shift();
+	try{
+		stream.write(`${JSON.stringify(log)}\n`);
+		// only push to transports if logLevel is greater than or equal to 4 (warn)
+		if (log._meta.logLevelId >= 4) {
+			const logEvent: logEvent = {
+				date : log._meta.date,
+				severity: log._meta.logLevelName,
+				message: log['0'].toString(),
+			};
+			logHistory.push(logEvent);
+			// Keep only the last 1000 lines in logHistory
+			if (logHistory.length > 1000) {
+				logHistory.shift();
+			}
 		}
+	}catch(e){
+		logger.fatal("Can't write to log file");
 	}
+	
 });
 
 export { logger, logHistory };
