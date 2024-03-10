d695833deaea9996453127035e69366bef41a146
quentintaranpino <quentintaranpino@proton.me>
2024-03-10 17:45:54 +0100

    new lib/server.ts only for server.ts things

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,49 +1,4 @@
-import { logger } from "./logger.js";
-import path from 'path';
-import url from 'url';
-import markdownit from 'markdown-it';
 import { Application } from "express";
-import { Request } from "express";
-
-const getClientIp = (req: Request): string => {
-    let ip = req.headers['x-forwarded-for'];
-    if (Array.isArray(ip)) {
-        ip = ip[0];
-    } else {
-        ip = ip || req.connection.remoteAddress;
-    }
-    if (typeof ip === 'string' && ip.startsWith("::ffff:")) {
-        ip = ip.substring(7);
-    }
-    return ip || "";
-};
-
-const format = (seconds:number):string =>{
-	function pad(s: number){
-		return (s < 10 ? '0' : '') + s;
-	}
-	const hours = Math.floor(seconds / (60*60));
-	const minutes = Math.floor(seconds % (60*60) / 60);
-	const secs = Math.floor(seconds % 60);
-  
-	return pad(hours) + ':' + pad(minutes) + ':' + pad(secs);
-}
-
-const currDir = (fileUrl:string) : string =>{
-    const __filename = url.fileURLToPath(fileUrl);
-    return path.dirname(__filename);
-}
-
-const markdownToHtml = (text:string) : string => {
-
-    const md = markdownit();
-    try{
-        return md.render(text).toString();
-    }catch(err){
-        logger.error("Error parsing markdown to html: ", err);
-        return "";
-    }
-}
 
 const loadConsoleBanner = (app: Application) : void => {
 
@@ -87,10 +42,9 @@ const loadConsoleBanner = (app: Application) : void => {
 	console.log(
 		"╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝"
 	);
-
 	console.log("Nostrcheck server started, version %s", app.get("version"));
 	console.log("Running at http://" + app.get("config.server")["host"] + " - " + app.get("env"), "mode");
 	console.log("Press CTRL-C to exit\n");
 }
 
-export { getClientIp, format, currDir, markdownToHtml, loadConsoleBanner};
+export { loadConsoleBanner};
