2f3d4a6bcee78cd42b977bbceceaf1d5a7968637
quentintaranpino <quentintaranpino@proton.me>
2024-03-11 15:11:05 +0100

    remove old code from init.ts

--- src/init.ts
+++ src/init.ts
@@ -1,19 +1,14 @@
-// init.ts
 import fs from "fs";
 import { defaultConfig, localPath } from "./interfaces/config.js";
 
 (async () => {
-    console.log("Checking local config file: " + localPath);
     if (!fs.existsSync(localPath)){
         console.warn("Local config file not found. Creating new one.");
         try {
             fs.writeFileSync(localPath, JSON.stringify(defaultConfig, null, 2));
-            console.info("Creating local config file: " + localPath);
-            console.info("Please edit the file and restart the server");
-            process.exit(1);
+            console.info("Creating local config file: " + localPath, "Please edit the file and restart the server");
         } catch (err) {
             console.error("An error occured while writing config JSON File.", err);
-            process.exit(1);
         }
     }
     await new Promise((resolve) => setTimeout(resolve, 1));
