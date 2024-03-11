48736a56424507250590fe69250255314fb41fa6
quentintaranpino <quentintaranpino@proton.me>
2024-03-11 15:22:35 +0100

    create config dir if not exist on init

--- src/init.ts
+++ src/init.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
+import path from "path";
 import { defaultConfig, localPath } from "./interfaces/config.js";
 
 (async () => {
     if (!fs.existsSync(localPath)){
         console.warn("Local config file not found. Creating new one.");
         try {
+            fs.mkdirSync(path.dirname(localPath), { recursive: true });
             fs.writeFileSync(localPath, JSON.stringify(defaultConfig, null, 2));
             console.info("Creating local config file: " + localPath, "Please edit the file and restart the server");
         } catch (err) {
