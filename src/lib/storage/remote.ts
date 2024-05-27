b793e1fb40cb8a53dcdb2203867763cabe9f1daf
quentintaranpino <quentintaranpino@proton.me>
2024-05-27 15:29:08 +0200

    remove config references from remote.ts

--- src/lib/storage/remote.ts
+++ src/lib/storage/remote.ts
@@ -1,24 +1,25 @@
 import fs from 'fs';
-import config from "config";
 import { S3Client, S3ClientConfig, PutObjectCommand } from "@aws-sdk/client-s3";
 import { ProcessingFileData, mime_conversion } from '../../interfaces/media.js';
 import { logger } from '../logger.js';
+import app from '../../app.js';
 
 const s3Config: S3ClientConfig = {
-  endpoint: config.get("storage.remote.endpoint"),
+
+  endpoint: app.get("config.storage")["remote"]["endpoint"] ,
   credentials: {
-    accessKeyId: config.get("storage.remote.accessKeyId"),
-    secretAccessKey: config.get("storage.remote.secretAccessKey"),
+    accessKeyId: app.get("config.storage")["remote"]["accessKeyId"],
+    secretAccessKey: app.get("config.storage")["remote"]["secretAccessKey"],
   },
-  region: config.get("storage.remote.region"),
-  forcePathStyle: config.get("storage.remote.s3ForcePathStyle"),
+  region: app.get("config.storage")["remote"]["region"],
+  forcePathStyle: app.get("config.storage")["remote"]["s3ForcePathStyle"],
 };
 
 const s3Client = new S3Client(s3Config);
 
 const saveFileS3 = async (filePath: string, filedata:ProcessingFileData): Promise<string> => {
 
-  const bucketName :string = config.get("storage.remote.bucketName");
+  const bucketName :string = app.get("config.storage")["remote"]["bucketName"];
 
   const params = {
     Bucket: bucketName,
@@ -30,7 +31,7 @@ const saveFileS3 = async (filePath: string, filedata:ProcessingFileData): Promis
   logger.debug(params.ContentType)
   try {
     await s3Client.send(new PutObjectCommand(params));
-    const fileUrl = `${config.get("storage.remote.endpoint")}/${params.Bucket}/${params.Key}`;
+    const fileUrl = `${app.get("config.storage")["remote"]["endpoint"]}/${params.Bucket}/${params.Key}`;
     logger.info(`Successfully uploaded file to ${fileUrl}`);
     return fileUrl;
   } catch (error) {
