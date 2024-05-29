6369fc53fcf68de402c1f6e9662bcd1177bdac37
quentintaranpino <quentintaranpino@proton.me>
2024-05-29 11:48:18 +0200

    sync default config values on init.ts

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import { defaultConfig, localPath } from "./interfaces/config.js";
 import { exit } from "process";
+import { syncDefaultConfigValues } from "./lib/config.js";
 
 console.debug("Starting NostrCheck API Server", );
 (async () => {
@@ -11,12 +12,17 @@ console.debug("Starting NostrCheck API Server", );
             fs.mkdirSync(path.dirname(localPath), { recursive: true });
             fs.writeFileSync(localPath, JSON.stringify(defaultConfig, null, 2));
             console.info("Creating local config file: " + localPath, "Please edit the file and restart the server");
+            exit(0);
         } catch (err) {
             console.error("An error occured while writing config JSON File.", err);
             exit(1);
         }
     }
-    await new Promise((resolve) => setTimeout(resolve, 1));
+    if(await syncDefaultConfigValues(defaultConfig, localPath)){
+        console.info("Config file updated with new default fields. Please restart the server");
+        exit(0);
+    }
+    
     const startServer = await import('./server.js');
     await startServer.default();
 })();
