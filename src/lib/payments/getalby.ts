385521ac0538278d92e17c1e6ae800a9c4d61fc1
quentintaranpino <quentintaranpino@proton.me>
2024-05-30 08:33:59 +0200

    testing getalby invoice generation

--- src/lib/payments/getalby.ts
+++ src/lib/payments/getalby.ts
@@ -1,2 +1,25 @@
 // https://github.com/getAlby/js-sdk
+// https://github.com/getAlby/js-lightning-tools
+import { LightningAddress } from "@getalby/lightning-tools";
 
+const generateGetalbyInvoice = async () => {
+    const ln = new LightningAddress("nostrcheckme@getalby.com");
+    await ln.fetch();
+
+    const invoice = await ln.requestInvoice({ satoshi: 1 });
+    console.log(invoice.paymentRequest); 
+
+    while (!await invoice.isPaid()) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        console.log("Waiting for payment...");
+    }
+
+    const paid = await invoice.verifyPayment(); 
+    if (paid) {
+        console.log(invoice.preimage);
+    }
+
+    return invoice;
+}
+
+export { generateGetalbyInvoice }
