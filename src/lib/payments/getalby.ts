255a0eb446e5a934e4e5ac3eae20f61f0f494e0a
quentintaranpino <quentintaranpino@proton.me>
2024-06-03 10:42:07 +0200

    add try catch to getAlby methods

--- src/lib/payments/getalby.ts
+++ src/lib/payments/getalby.ts
@@ -3,50 +3,67 @@
 import { LightningAddress } from "@getalby/lightning-tools";
 import { invoice } from "../../interfaces/payments.js";
 import app from "../../app.js";
+import { logger } from "../logger.js";
 
 const generateGetalbyInvoice = async (LNAddress: string, amount:number) : Promise<invoice> => {
+
     const ln = new LightningAddress(LNAddress);
-    await ln.fetch();
 
-    const getalbyInvoice = await ln.requestInvoice({ satoshi: amount });
+    try{
+        await ln.fetch();
+        const getalbyInvoice = await ln.requestInvoice({ satoshi: amount });
+        return getalbyInvoice;
+    }catch(e){
+        logger.error("Error generating invoice", e);
+        return {paymentRequest: "", paymentHash: "", createdDate: "", expiryDate: "", description: "", isPaid: async () => {return false}, verifyPayment: async () => {return false}};
+    }
 
-    return getalbyInvoice;
 }
 
 const isInvoicePaid = async (paymentHash: string) : Promise<boolean> => {
 
     const authToken = app.get("config.payments")["getalby"]["authToken"];
 
-    const response = await fetch(`https://api.getalby.com/invoices/${paymentHash}`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${authToken}`
+    try{
+        const response = await fetch(`https://api.getalby.com/invoices/${paymentHash}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        });
+
+        const data = await response.json();
+
+        if (data.preimage && data.preimage != "null") {
+            return true;
         }
-    });
 
-    const data = await response.json();
-
-    if (data.preimage && data.preimage != "null") {
-        return true;
+        return false;
+    }catch(e){
+        logger.error("Error checking invoice status", e);
+        return false;
     }
-
-    return false;
 }
 
 const getInvoiceQR = async (paymentHash: string) : Promise<string> => {
 
     const authToken = app.get("config.payments")["getalby"]["authToken"];
 
-    const response = await fetch(`https://api.getalby.com/invoices/${paymentHash}`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${authToken}`
-        }
-    });
+    try{
+        const response = await fetch(`https://api.getalby.com/invoices/${paymentHash}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            }
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    return data.qr_code_png;
+        return data.qr_code_png;
+    }catch(e){
+        logger.error("Error getting invoice QR code", e);
+        return "";
+    }
 
 }
 
