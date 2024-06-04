73347780153f2b33df5a76395031716ef78fffd7
quentintaranpino <quentintaranpino@proton.me>
2024-06-04 11:17:17 +0200

    refactor invoice interface

--- src/interfaces/payments.ts
+++ src/interfaces/payments.ts
@@ -4,8 +4,9 @@ interface invoice {
     createdDate: any;
     expiryDate: any;
     description: any;
-    isPaid: () => Promise<boolean>;
-    verifyPayment: () => Promise<boolean>;
+    isPaid: boolean;
+    transactionid: number;
+    satoshi: number;
 }
 
 interface checkPaymentResult {
