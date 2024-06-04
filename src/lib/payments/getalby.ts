c4b54fea80cc53b8596101141811ba851c755d7c
quentintaranpino <quentintaranpino@proton.me>
2024-06-04 11:18:02 +0200

    refactor getAlby methods

--- src/lib/payments/getalby.ts
+++ src/lib/payments/getalby.ts
@@ -12,15 +12,16 @@ const generateGetalbyInvoice = async (LNAddress: string, amount:number) : Promis
     try{
         await ln.fetch();
         const getalbyInvoice = await ln.requestInvoice({ satoshi: amount });
-        return getalbyInvoice;
+        return {paymentRequest: getalbyInvoice.paymentRequest, paymentHash: getalbyInvoice.paymentHash, createdDate: getalbyInvoice.createdDate, expiryDate: getalbyInvoice.expiryDate, description: getalbyInvoice.description, isPaid: false, satoshi: getalbyInvoice.satoshi, transactionid: 0};
+        
     }catch(e){
         logger.error("Error generating invoice", e);
-        return {paymentRequest: "", paymentHash: "", createdDate: "", expiryDate: "", description: "", isPaid: async () => {return false}, verifyPayment: async () => {return false}};
+        return {paymentRequest: "", paymentHash: "", createdDate: "", expiryDate: "", description: "", isPaid: false, satoshi: amount, transactionid: 0};
     }
 
 }
 
-const isInvoicePaid = async (paymentHash: string) : Promise<boolean> => {
+const isInvoicePaid = async (paymentHash: string) : Promise<string> => {
 
     const authToken = app.get("config.payments")["getalby"]["authToken"];
 
@@ -35,13 +36,12 @@ const isInvoicePaid = async (paymentHash: string) : Promise<boolean> => {
         const data = await response.json();
 
         if (data.preimage && data.preimage != "null") {
-            return true;
+            return data.settled_at;
         }
-
-        return false;
+        return "";
     }catch(e){
         logger.error("Error checking invoice status", e);
-        return false;
+        return "";
     }
 }
 
