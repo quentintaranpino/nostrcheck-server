4523c374fa7f9ff6a0e1ceabdb0c6b4f755d7f6c
quentintaranpino <quentintaranpino@proton.me>
2024-06-12 09:51:24 +0200

    fix: smooth scroll

--- src/pages/static/js/server.js
+++ src/pages/static/js/server.js
@@ -15,20 +15,38 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 
 // Smooth scroll and offset by 200px
+function smoothScroll(target, duration) {
+  const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - 200;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        const offsetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - 200;
-
-        window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-    });
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      smoothScroll(targetElement, 1000); 
+  });
 });
-
 
 // Theme switcher
 const themeSelector = document.getElementById('theme-selector');
