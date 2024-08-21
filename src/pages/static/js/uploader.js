bd7b3edde695965ddc0a7801b55270658e574b88
quentintaranpino <quentintaranpino@proton.me>
2024-08-21 11:03:05 +0200

    CDN page refactor. Now accept "transform" (NIP96/Blossom) for each file.

--- src/pages/static/js/uploader.js
+++ src/pages/static/js/uploader.js
@@ -1,25 +1,8 @@
-const uploadButton = async () => {
-    return new Promise((resolve) => {
-        var input = $(document.createElement("input"));
-        input.attr("type", "file");
-        input.attr("multiple", true);
-        input.trigger("click"); 
+const fetchFileServer = async (file, authEventPut = "", authEventGet = "", method = "", showMessages = false) => {
 
-        input.on("cancel", function () {
-            resolve("File upload cancelled");
-        });
-
-        input.on("change", async function (e) {
-
-            for (const file of e.target.files) {
-                const result = await fetchFileServer(file);
-                resolve(result);
-            }
-        });
-    });
-}
-
-const fetchFileServer = async (file, authEventPut = "", authEventGet = "") => {
+    if (!method) {
+        return;
+    }
 
     const mimeDB = await fetch('https://cdn.jsdelivr.net/gh/jshttp/mime-db/db.json')
         .then(res => res.json())
@@ -54,21 +37,26 @@ const fetchFileServer = async (file, authEventPut = "", authEventGet = "") => {
 
     let headers = {};
     headers["authorization"] = localStorage.getItem('authkey') != "" ? "Bearer " + localStorage.getItem('authkey') : "Nostr " + authEventPut;
-    headers["Content-Type"] = mimeType;
-    const uploadMessage = showMessage(`Uploading file... `, "alert-info", true);
+    method == "PUT" ? headers["Content-Type"] = mimeType : null;
+    let uploadMessage = null
+    
+    if (showMessages) uploadMessage = showMessage(`Uploading file... `, "alert-info", true);
+
+    const formData = new FormData();
+    formData.append('file', file);
 
     try {
-        const response = await fetch('https://cdn.' + getDomain(window.location.hostname) + '/upload/', {
-            method: "PUT",
+        const response = await fetch(method == "PUT" ? '/api/v2/media/upload' : '/api/v2/media', {
+            method: method,
             headers: headers,
-            body: file
+            body: method == "PUT" ? file : formData
         });
 
         let serverData = await response.json();
 
         if (response.status.toString().startsWith("4")) {
-            updateMessage(uploadMessage, '<i class="bi bi-exclamation-circle-fill pe-1"></i>' + serverData.message, "alert-danger");
-            hideMessage(uploadMessage, 5000);
+            if (showMessages) updateMessage(uploadMessage, '<i class="bi bi-exclamation-circle-fill pe-1"></i>' + serverData.message, "alert-danger");
+            if (showMessages) hideMessage(uploadMessage, 5000);
             await storeAuthkey('', true);
             return { "filename": file.name, "url": "" };
         }
@@ -88,14 +76,14 @@ const fetchFileServer = async (file, authEventPut = "", authEventGet = "") => {
             const processingData = await processingResponse.json();
 
             if (processingData.status === "processing") {
-                updateMessage(uploadMessage, `Processing file... ${processingData.percentage}`);
+                if (showMessages) updateMessage(uploadMessage, `Processing file... ${processingData.percentage}`);
             } else {
                 serverData.processing_url = "";
             }
         }
 
-        const url = serverData.nip94_event ? serverData.nip94_event.tags.find(tag => tag[0] === "url")[1] : serverData.processing_url;
-        updateMessage(uploadMessage,
+        const url = serverData.nip94_event ? serverData.nip94_event.tags.find(tag => tag[0] === "url")[1] : serverData.processing_url ? serverData.processing_url : serverData.url;
+        if (showMessages) updateMessage(uploadMessage,
             `File uploaded successfully!
             <button class="btn p-0 pb-1" type="button" aria-controls="intro" aria-selected="false" aria-label="View file">
                 <a href="${url}" target="_blank"><span><i class="fa-solid fa-link ms-2 me-2"></i><span class="visually-hidden">View file</span></span></a>
@@ -105,13 +93,13 @@ const fetchFileServer = async (file, authEventPut = "", authEventGet = "") => {
             </button>
             `
         );
-        hideMessage(uploadMessage, 15000);
+        if (showMessages) hideMessage(uploadMessage, 15000);
         console.log(url);
         return { "filename": file.name, "url": url };
 
     } catch (error) {
-        updateMessage(uploadMessage, '<i class="bi bi-exclamation-circle-fill pe-1"></i>' + error, "alert-danger");
-        hideMessage(uploadMessage, 5000);
+        if (showMessages) updateMessage(uploadMessage, '<i class="bi bi-exclamation-circle-fill pe-1"></i>' + error, "alert-danger");
+        if (showMessages) hideMessage(uploadMessage, 5000);
         await storeAuthkey('', true);
         return { "filename": file.name, "url": "" };
     }
