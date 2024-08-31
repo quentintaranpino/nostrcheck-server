0a72d47ba5656d30aafada1f4d0b2d92954b4bfd
quentintaranpino <quentintaranpino@proton.me>
2024-08-31 11:07:10 +0200

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -253,7 +253,7 @@ jq -n --arg a "$HOST" --arg b "$PUBKEY" --arg c "$SECRETKEY" --arg d "$DB" --arg
         "host": $a,
         "port": 3000,
         "pubkey": $b,
-        "secretKey": $c
+        "secretKey": $c,
         "tosFilePath": "resources/tos.md"
     },
     "database": {
