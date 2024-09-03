68160f35c3595499cf9df7d82d04c627117b66b2
quentintaranpino <quentintaranpino@proton.me>
2024-09-03 12:01:44 +0200

    update package version

--- update-version.js
+++ update-version.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { execSync } from 'child_process';
 
 const packageJsonPath = path.resolve(process.cwd(), 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
@@ -16,6 +17,8 @@ if (versionParts.length > 1) {
 buildNumber += 1;
 
 versionParts[versionParts.length - 1] = buildNumber.toString();
-packageJson.version = versionParts.join('.');	
+packageJson.version = versionParts.join('.');
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf-8');
+
+execSync('npm install', { stdio: 'inherit' });
