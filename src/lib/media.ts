adfd779c86a5714f0a0c5eadceed177afa8340f6
quentintaranpino <quentintaranpino@proton.me>
2024-09-09 12:53:49 +0200

    remove checkTransaction from media transform lib.

--- src/lib/media.ts
+++ src/lib/media.ts
@@ -411,9 +411,6 @@ const finalizeFileProcessing = async (filedata: fileData): Promise<boolean> => {
 
 		if (filedata.no_transform == false) { await deleteLocalFile(filedata.conversionOutputPath);}
 		await deleteLocalFile(filedata.conversionInputPath);
-
-		await checkTransaction(filedata.transaction_id, filedata.fileid, "mediafiles", filesize, filedata.pubkey);
-
 		moderateFile(filedata.url).then((result) => {
 			result.code == "NA"? dbUpdate('mediafiles','checked','1',['id'], [filedata.fileid]): null;
 		}).catch((err) => {
