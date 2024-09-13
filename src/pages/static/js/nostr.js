0b489e8c2de2e8d1dc291b7d1de7ded7a1d73c30
quentintaranpino <quentintaranpino@proton.me>
2024-09-13 14:55:53 +0200

    user relays tab. refactor nostr tools

--- src/pages/static/js/nostr.js
+++ src/pages/static/js/nostr.js
@@ -1,150 +1,195 @@
-// let sk = "";
-// sk = NostrTools.nip19.decode(sk).data;
-// const pk = NostrTools.getPublicKey(sk);
-// console.log("PUKBEY:", pk);
+// Initialize the SimplePool from NostrTools
+const pool = new NostrTools.SimplePool();
 
-const pool = new NostrTools.SimplePool()
+// Default relay configuration
+let relays = [
+  { id: 0, url: 'wss://relay.nostr.band', type: 'write', name:'', description: "", pubkey: "", contact: "", supported_nips: [] },
+];
 
-let relays = [{url:'wss://relay.damus.io', type: 'write'}, {url:'wss://nos.lol', type: 'write'}, {url:'wss://relay.nostrcheck.me', type: 'read'}];
+/**
+ * Fetches the user's preferred relays from their Nostr profile (kind 10002 events).
+ * @param {string} publicKey - The user's public key.
+ * @returns {Promise<Array>} - A promise that resolves to an array of relay objects.
+ */
+const getRelaysFromUser = async (publicKey) => {
+  return new Promise((resolve, reject) => {
+    const userRelays = [];
 
-const getRelaysFromUser = async (pk) => {
-    let userRelays = [];
-
-    return new Promise((resolve, reject) => {
-        try {
-            let p = pool.subscribeMany(
-                relays.map(relay => relay.url),
-                [
-                    {
-                        kinds: [10002], 
-                        authors: [pk], 
-                    },
-                ],
-                {
-                    async onevent(event) {
-                        console.log("Event received (kind 10002):", event);
-
-                        event.tags.forEach(tag => {
-                            if (tag[0] === 'r') {
-                                console.log(tag[0], tag[1], tag[2]);
-                                userRelays.push({ url: tag[1], type: tag[2] || "read / write" });
+    try {
+      const subscription = pool.subscribeMany(
+        relays.map(relay => relay.url),
+        [{ kinds: [10002], authors: [publicKey] }],
+        {
+          async onevent(event) {
+            console.log("Event received (kind 10002):", event);
+            let id = 0;
+            event.tags.forEach(tag => {
+              if (tag[0] === 'r') {
+                userRelays.push({id : id, url: tag[1], type: tag[2] || 'read/write', name: '', description: '', pubkey: '', contact: '', supported_nips: [] });
+                id++;
+              }
+            });
+          },
+          oneose() {
+            subscription.close();
+            (async () => {
+                for (const relay of userRelays) {
+                    try {
+                        const response = await fetch(relay.url.replace('wss://', 'https://').replace('ws://', 'http://'), {
+                            headers: {
+                                'Accept': 'application/nostr+json'
                             }
                         });
-                    },
-
-                    oneose() {
-                        p.close();
-                        console.log('User relays:', userRelays);
-                        resolve(userRelays); 
+                        const data = await response.json();
+                        relay.name = data.name;
+                        relay.description = data.description;
+                        relay.pubkey = data.pubkey;
+                        relay.contact = data.contact;
+                        relay.supported_nips = data.supported_nips;
+                        relay.pubkey = data.pubkey;
+                    } catch (error) {
+                        console.warn('Error fetching relay data:', error);
                     }
                 }
-            );
+                console.log('User relays:', userRelays);
+            
+                resolve(userRelays);
+            })();
+          }
+        }
+      );
+    } catch (error) {
+      console.error("Error obtaining user relays:", error);
+      reject(error);
+    }
+  });
+};
 
-        } catch (e) {
-            p.close();
-            console.error("Error obtaining user relays:", e);
-            reject(e); 
+/**
+ * Checks if a relay is online.
+ * @param {string} relayUrl - The URL of the relay to check.
+ * @returns {Promise<Object>} - A promise that resolves to an object with the online status and response time.
+ */
+const isRelayOnline = async (relayUrl) => {
+    return new Promise(async (resolve) => {
+        const startTime = Date.now(); // Tiempo de inicio
+        try {
+            await NostrTools.Relay.connect(relayUrl);
+            const responseTime = Date.now() - startTime; 
+            resolve({ online: true, ping: responseTime });
+        } catch (error) {
+            resolve({ online: false, ping: null });
         }
     });
 };
 
+/**
+ * Ensures that the profile content has at least the minimum required fields.
+ * @param {Object} content - The profile content to ensure.
+ * @returns {Object} - The content with minimum required fields ensured.
+ */
 const ensureMinimumFields = (content) => {
-    const defaultFields = {
-        name: "",
-        banner: null,
-        picture: null,
-        website: "",
-        lud16: "",
-        nip05: ""
-    };
-    
-    return { ...defaultFields, ...content }; 
+  const defaultFields = {
+    name: "",
+    banner: null,
+    picture: null,
+    website: "",
+    lud16: "",
+    nip05: ""
+  };
+  return { ...defaultFields, ...content };
 };
 
-const publishProfileData = async (updatedFields, pk, sk) => {
-    if (!updatedFields || Object.keys(updatedFields).length === 0 || !pk) {
-        console.error("No hay campos actualizados.");
-        return;
-    }
+/**
+ * Publishes the updated profile data to the user's relays.
+ * @param {Object} updatedFields - The updated fields to merge into the profile.
+ * @param {string} publicKey - The user's public key.
+ * @param {string} secretKey - The user's secret key.
+ * @returns {Promise<boolean>} - A promise that resolves to true if publishing was successful.
+ */
+const publishProfileData = async (updatedFields, publicKey, secretKey) => {
+  if (!updatedFields || Object.keys(updatedFields).length === 0 || !publicKey) {
+    console.error("No updated fields or public key provided.");
+    return;
+  }
 
-    console.log("Default relays:", relays.map(relay => relay.url));
-    let userRelays =  await getRelaysFromUser(pk);
-    if (!userRelays || userRelays.length === 0) {
-        console.warn("No relays found for user, using default relays.");
-    } else {
-        relays = userRelays;
-    }
-    console.log("Relays to use:", relays.map(relay => relay.url));
+  console.log("Default relays:", relays.map(relay => relay.url));
 
-    return new Promise((resolve, reject) => {
-        let combinedContent = {};
+  let userRelays = await getRelaysFromUser(publicKey);
+  if (!userRelays || userRelays.length === 0) {
+    console.warn("No relays found for user, using default relays.");
+  } else {
+    relays = userRelays;
+  }
 
-        const p = pool.subscribeMany(
-            relays.map(relay => relay.url), 
-            [
-                {
-                    kinds: [0], 
-                    authors: [pk],
-                },
-            ],
-            {
-                async onevent(event) {
-                    let eventContent = {};
-                    try {
-                        eventContent = JSON.parse(event.content);
-                        console.log("Received event:", eventContent);
-                        console.log(event);
-                    } catch (e) {
-                        console.error("Error parsing event content:", e);
-                        reject("Error parsing event content");
-                    }
+  console.log("Relays to use:", relays.map(relay => relay.url));
 
-                    combinedContent = { ...combinedContent, ...eventContent };
-                },
+  return new Promise((resolve, reject) => {
+    let combinedContent = {};
 
-                async oneose() {
-                    p.close();
-                    const updatedContent = ensureMinimumFields({ ...combinedContent, ...updatedFields });
-
-                    const event = {
-                        kind: 0,
-                        created_at: Math.floor(Date.now() / 1000),
-                        tags: [],
-                        content: JSON.stringify(updatedContent),
-                    };
-
-                    let newEvent;
-                    try{
-                        if (sk) {
-                            sk = NostrTools.nip19.decode(sk).data;
-                            newEvent = await NostrTools.finalizeEvent(event, sk)
-                        }else{
-                            newEvent = await window.nostr.signEvent(event);
-                        }
-                    }catch(e){ 
-                        console.error("Error signing event:", e);
-                        reject(false);
-                    }
-                    console.log("New event:", newEvent);
-                    if (!NostrTools.verifyEvent(newEvent)) {
-                        console.error("Combined event is not valid.");
-                        reject(false);
-                    }
-
-                    console.log("New event generated:", newEvent);
-
-                    await Promise.any(pool.publish(relays.map(relay => relay.url), newEvent))
-                        .then((e) => {
-                            console.log(e);
-                            console.log("Event published successfully.");
-                            resolve(true); 
-                        })
-                        .catch((err) => {
-                            console.error("Failed to publish event:", err);
-                            reject(false); 
-                        });
-                },
+    try {
+      const subscription = pool.subscribeMany(
+        relays.map(relay => relay.url),
+        [{ kinds: [0], authors: [publicKey] }],
+        {
+          async onevent(event) {
+            try {
+              const eventContent = JSON.parse(event.content);
+              console.log("Received event:", eventContent);
+              combinedContent = { ...combinedContent, ...eventContent };
+            } catch (error) {
+              console.error("Error parsing event content:", error);
+              reject("Error parsing event content");
             }
-        );
-    });
+          },
+          async oneose() {
+            subscription.close();
+            const updatedContent = ensureMinimumFields({ ...combinedContent, ...updatedFields });
+
+            const event = {
+              kind: 0,
+              created_at: Math.floor(Date.now() / 1000),
+              tags: [],
+              content: JSON.stringify(updatedContent),
+              pubkey: publicKey
+            };
+
+            let signedEvent;
+            try {
+              if (secretKey) {
+                const decodedSecretKey = NostrTools.nip19.decode(secretKey).data;
+                signedEvent = await NostrTools.finalizeEvent(event, decodedSecretKey);
+              } else if (window.nostr && window.nostr.signEvent) {
+                signedEvent = await window.nostr.signEvent(event);
+              } else {
+                throw new Error("No secret key provided and window.nostr is not available.");
+              }
+            } catch (error) {
+              console.error("Error signing event:", error);
+              reject(error);
+            }
+
+            if (!NostrTools.verifyEvent(signedEvent)) {
+              console.error("Signed event is not valid.");
+              reject("Signed event is not valid.");
+            }
+
+            console.log("Signed event generated:", signedEvent);
+
+            try {
+              await Promise.any(pool.publish(relays.map(relay => relay.url), signedEvent));
+              console.log("Event published successfully.");
+              resolve(true);
+            } catch (error) {
+              console.error("Failed to publish event:", error);
+              reject(error);
+            }
+          },
+        }
+      );
+    } catch (error) {
+      console.error("Error during publishing profile data:", error);
+      reject(error);
+    }
+  });
 };
