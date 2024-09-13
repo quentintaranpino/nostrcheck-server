e300f496101db2b3e741196ae8dd8385dde3ed3a
quentintaranpino <quentintaranpino@proton.me>
2024-09-13 18:46:02 +0200

    remove nostr notes retrieval from server. Now client side

--- src/controllers/frontend.ts
+++ src/controllers/frontend.ts
@@ -6,7 +6,7 @@ import { getClientIp, markdownToHtml } from "../lib/utils.js";
 import { dbSelect} from "../lib/database.js";
 import { generateCredentials, isAuthkeyValid, isPubkeyAllowed, isPubkeyValid, isUserPasswordValid } from "../lib/authorization.js";
 import { isModuleEnabled, loadconfigActiveModules } from "../lib/config.js";
-import { countPubkeyFiles, getProfileNostrMetadata, getProfileNostrNotes } from "../lib/frontend.js";
+import { countPubkeyFiles, getProfileNostrMetadata } from "../lib/frontend.js";
 import { hextoNpub } from "../lib/nostr/NIP19.js";
 import { logHistory } from "../lib/logger.js";
 import { themes, particles} from "../interfaces/themes.js";
@@ -105,9 +105,6 @@ const loadProfilePage = async (req: Request, res: Response, version:string): Pro
     // User metadata
     req.session.metadata = await getProfileNostrMetadata(req.session.identifier);
     req.session.metadata.usernames = await getUsernames(req.session.identifier);
-
-    // User nostr notes
-    req.session.metadata.nostr_notes = await getProfileNostrNotes(req.session.identifier);
 
     // User uploaded files
     req.session.metadata.hostedFiles =  await countPubkeyFiles(req.session.identifier);
