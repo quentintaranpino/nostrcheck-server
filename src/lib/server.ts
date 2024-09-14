d7ae1741185798e544367f2260954ce43fcfea03
quentintaranpino <quentintaranpino@proton.me>
2024-09-14 14:59:06 +0200

    fix banner version number

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -44,8 +44,8 @@ const serverBanner = (app: Application) : string => {
 	banner.push(
 		"╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝"
 	);
-	banner.push("Nostrcheck server started, version %s", app.get("version"));
-	banner.push(`Running at http://127.0.0.1:${app.get("config.server")["port"]} in ${app.get("config.environment")} mode`);
+	banner.push(`Nostrcheck server started, version ${app.get("version")}`);
+	banner.push(`Running at http://localhost:${app.get("config.server")["port"]} in ${app.get("config.environment")} mode`);
 	banner.push(`Documentation: https://github.com/quentintaranpino/nostrcheck-server/blob/main/DOCS.md`)
 	banner.push("");
 	banner.push("Press CTRL-C to stop the server");
