7d7a285071dabf5301ec77be9a8d49252df0d792
quentintaranpino <quentintaranpino@proton.me>
2024-09-17 12:19:34 +0200

    rename update-version to package

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
+import process from 'process';
 
 const packageJsonPath = path.resolve(process.cwd(), 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
