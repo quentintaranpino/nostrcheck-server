cde58dfe1298e1598f38ff318ae9f7cab0dff50c
quentintaranpino <quentintaranpino@proton.me>
2025-02-05 11:18:33 +0000

    add relay subdomain to installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -673,6 +673,33 @@ server {
         proxy_set_header Connection "upgrade";
     }
 }
+
+# Additional server block for relay.$HOST
+server {
+    listen 80;
+    server_name relay.$HOST;
+
+    location / {
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host \$host;
+        proxy_pass http://127.0.0.1:3000/api/v2/relay;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+
+    location /static {
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header Host \$host;
+        proxy_pass http://127.0.0.1:3000/static;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+    }
+}
+
 EOF
 
 if [ -f /etc/nginx/sites-available/$HOST.conf ]; then
