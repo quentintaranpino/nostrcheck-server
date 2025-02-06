cbc531bf402a018deebea921e9f5e1004a42cd28
quentintaranpino <quentintaranpino@proton.me>
2025-02-06 14:01:35 +0100

    update allowed tags

--- src/interfaces/relay.ts
+++ src/interfaces/relay.ts
@@ -10,6 +10,41 @@ interface ExtendedWebSocket extends WebSocket {
   challenge?: string;
 }
 
-const allowedTags = ["p", "e", "r", "t", "i", "d", "l", "L", "title", "nonce", "alt", "emoji", "subject", "expiration", "delegation", "proxy"];
+const allowedTags = [
+  "a",
+  "p",
+  "e",
+  "r",
+  "t",
+  "i",
+  "d",
+  "l",
+  "L",
+  "title",
+  "nonce",
+  "alt",
+  "emoji",
+  "subject",
+  "expiration",
+  "delegation",
+  "proxy",
+  "group",
+  "relay",
+  "word",
+  "pin",
+  "mute",
+  "bookmark",
+  "communities",
+  "start",
+  "end",
+  "location",
+  "g",
+  "start_tzid",
+  "end_tzid",
+  "summary",
+  "image",
+  "status",
+  "fb"
+];
 
 export { MemoryEvent, ExtendedWebSocket, allowedTags };
