ecbcaf13d9626c05fd34f57492fec928fd09b26c
quentintaranpino <quentintaranpino@proton.me>
2025-02-08 11:12:01 +0100

    load 50000 events test

--- src/lib/relay/database.ts
+++ src/lib/relay/database.ts
@@ -15,7 +15,7 @@ const initEvents = async (app: Application): Promise<boolean> => {
 
         const loadEvents = async () => {
             try {
-                const limit = 1000;
+                const limit = 50000;
                 let offset = 0;
                 let hasMore = true;
 
