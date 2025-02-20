a1552981b10c10096b83b71a66455611abaadd22
quentintaranpino <quentintaranpino@proton.me>
2025-02-20 12:39:29 +0100

    add NIP62 to supported nips

--- src/interfaces/nostr.ts
+++ src/interfaces/nostr.ts
@@ -205,7 +205,7 @@ interface AuthEvent {
   sig: string;
 }
 
-const supported_nips = [1, 2, 3, 4, 5, 7, 9, 11, 13, 14, 19, 40, 44, 47, 48, 50, 70, 78, 94, 96, 98];
+const supported_nips = [1, 2, 3, 4, 5, 7, 9, 11, 13, 14, 19, 40, 44, 47, 48, 50, 62, 70, 78, 94, 96, 98];
 
 export {
           supported_nips,
