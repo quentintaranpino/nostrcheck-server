eb583d21a619d5e2342fbcb972f67dfae2708dc6
quentintaranpino <quentintaranpino@proton.me>
2025-02-23 17:21:07 +0100

    add origin getHostInfo().url to admin routes

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,10 +1,13 @@
 import { Application } from "express";
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import { logger } from "../lib/logger.js";
 import { getClientIp } from "../lib/security/ips.js";
 import app from "../app.js";
 import { limiter } from "../lib/security/core.js";
+import { getHostInfo } from "../lib/utils.js";
+
 
 import {
     deleteDBRecord,
@@ -23,6 +26,11 @@ import {
     updateRelayIcon
 } from "../controllers/admin.js";
 
+const adminCORS = {
+    origin: getHostInfo().url,
+    Credentials: true,
+}
+
 const maxMBfilesize: number = app.get("config.media")["maxMBfilesize"];
 
 const upload = multer({
@@ -35,49 +43,56 @@ export const loadAdminEndpoint = async (app: Application, version: string): Prom
     if (version == "v2") {
 
         // Stop the server
-        app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/stop", limiter(), StopServer);
+        app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/stop", limiter(), cors(adminCORS), StopServer);
 
         // Legacy status endpoint
-        app.get("/api/" + version + "/status", limiter(), (_req, res) => {
+        app.get("/api/" + version + "/status", limiter(), cors(adminCORS), (_req, res) => {
             res.redirect("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/status");
         });
 
         // Get server status
-        app.get("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/status", limiter(), serverStatus);
+        app.get("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/status", limiter(), cors(adminCORS), serverStatus);
 
         // Reset user password
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/resetpassword/",
             limiter(),
+            cors(adminCORS),
             express.json(), resetUserPassword);
 
         // Update a database record
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updaterecord/",
             limiter(),
+            cors(adminCORS),
             express.json(), updateDBRecord);
 
         // Delete a database record
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/deleterecord/",
             limiter(),
+            cors(adminCORS),
             express.json(), deleteDBRecord);
 
         // Insert a database record
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/insertrecord/",
             limiter(),    
+            cors(adminCORS),
             express.json(), insertDBRecord);
 
         // Moderate a database record
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/moderaterecord",
             limiter(),
+            cors(adminCORS),
             express.json(), moderateDBRecord);
 
         // Update settings
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updatesettings/",
             limiter(),
+            cors(adminCORS),
             express.json(), updateSettings);
 
         // Upload frontend logo (handles files)
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updatelogo/", 
             limiter(),
+            cors(adminCORS),
             function (req, res) {
             logger.debug("POST /api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updatelogo", "|", getClientIp(req));
             upload.any()(req, res, function (err) {
@@ -92,6 +107,7 @@ export const loadAdminEndpoint = async (app: Application, version: string): Prom
         // Upload frontend relay icon (handles files)
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updaterelayicon/", 
             limiter(),
+            cors(adminCORS),
             function (req, res) {
             logger.debug("POST /api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updaterelayicon", "|", getClientIp(req));
             upload.any()(req, res, function (err) {
@@ -104,15 +120,16 @@ export const loadAdminEndpoint = async (app: Application, version: string): Prom
         });
 
         // Update frontend theme
-        app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updatetheme/", limiter(), express.json({ limit: "1mb" }), updateTheme);
+        app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/updatetheme/", limiter(), cors(adminCORS), express.json({ limit: "1mb" }), updateTheme);
 
         // Get module data
-        app.get("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/moduledata", limiter(), getModuleData);
-        app.get("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/moduleCountdata", limiter(), getModuleCountData);
+        app.get("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/moduledata", limiter(), cors(adminCORS),getModuleData);
+        app.get("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/moduleCountdata", limiter(), cors(adminCORS), getModuleCountData);
 
         // Ban a remote source
         app.post("/api/" + version + app.get("config.server")["availableModules"]["admin"]["path"] + "/ban",
             limiter(),
+            cors(adminCORS),
             express.json(), banDBRecord);
     }
 
