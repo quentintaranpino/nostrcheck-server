5c88ea323f06da3ec04b5cb983b222841ece2dff
quentintaranpino <quentintaranpino@proton.me>
2025-02-25 16:01:34 +0100

    fix: relay dates on dashboard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -154,14 +154,14 @@ const generateVideoFromImage = (imageBuffer: Buffer): Promise<Buffer> => {
 };
 
 const getNewDate = (): string =>{
-const now = new Date();
-const year = now.getFullYear();
-const month = String(now.getMonth() + 1).padStart(2, '0'); // Meses son 0-11
-const day = String(now.getDate()).padStart(2, '0');
-const hours = String(now.getHours()).padStart(2, '0');
-const minutes = String(now.getMinutes()).padStart(2, '0');
-const seconds = String(now.getSeconds()).padStart(2, '0');
-return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+	const now = new Date();
+	const year = now.getFullYear();
+	const month = String(now.getMonth() + 1).padStart(2, '0'); 
+	const day = String(now.getDate()).padStart(2, '0');
+	const hours = String(now.getHours()).padStart(2, '0');
+	const minutes = String(now.getMinutes()).padStart(2, '0');
+	const seconds = String(now.getSeconds()).padStart(2, '0');
+	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 function isBase64(str: string): boolean {
