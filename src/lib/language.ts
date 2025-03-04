ef64809bed6f818785bda5bf2ea3a4b868f78cec
quentintaranpino <quentintaranpino@proton.me>
2025-03-04 15:47:13 +0100

    remove lande, add franc

--- src/lib/language.ts
+++ src/lib/language.ts
@@ -1,9 +1,9 @@
-import lande from 'lande';
+import { francAll } from 'franc';
 import { ISO_639_3_TO_1 } from '../interfaces/language.js';
 
 const getTextLanguage = (text: string): string => {
     if (text.length < 10) return '';
-    const languages = lande(text);
+    const languages = francAll(text);
     if (!languages.length) return '';
     return ISO_639_3_TO_1[languages[0][0]] || languages[0][0];
 }
