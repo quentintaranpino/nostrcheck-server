17481ec3d5b0fd609e7b08e5c95c233b22f72126
quentintaranpino <quentintaranpino@proton.me>
2025-09-12 22:14:30 +0200

    fix: update proxy_pass URL to include trailing slash for relay url

--- scripts/install.sh
+++ scripts/install.sh
@@ -637,7 +637,7 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_set_header Host \$host;
-        proxy_pass http://127.0.0.1:3000/api/v2/relay;
+        proxy_pass http://127.0.0.1:3000/api/v2/relay/;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
