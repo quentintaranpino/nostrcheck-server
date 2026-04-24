03b356d0506338e30ee7d37774c1c369323c7166
quentintaranpino <quentintaranpino@proton.me>
2026-04-24 17:57:49 +0200

    algorithms: ["HS256"]  for jwt

--- src/lib/authorization.ts
+++ src/lib/authorization.ts
@@ -203,7 +203,7 @@ const isAuthkeyValid = async (authString: string, checkAdminPrivileges: boolean 
     }
 
     try {
-		const decoded = jwt.verify(authString, getConfig(null, ["session", "secret"])) as { identifier: string, allowed: boolean, exp: number };
+		const decoded = jwt.verify(authString, getConfig(null, ["session", "secret"]), { algorithms: ["HS256"] }) as { identifier: string, allowed: boolean, exp: number };
 		if ((decoded.exp - Math.floor(Date.now() / 1000)) < 900){
 			authString = generateAuthToken(decoded.identifier, decoded.allowed);
 		} 
